4caaf2b7eb283d5afb5c2d3eb857a0d801f71f71
Gabriel Andrade do Nascimento <gabrielandradedonascimento963@gmail.com>
2025-11-27 13:11:54 -0300

    cleaning and organizang the currency data import

--- src/scraper.py
+++ src/scraper.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import requests
 import pandas as pd
+from decimal import Decimal
 
 from datetime import datetime, timedelta
 
@@ -48,47 +49,71 @@ def _find_date_intervals(dates_to_download: list[str]) -> list[tuple[str, str]]:
     return intervals
 
 def download_currency_data(partition_key: str, dates_to_download: list[str]):
-
-  base_url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/"
-  all_downloaded_data = []
-
-  # Extract currency code from partition key
-  currency = partition_key.split('#')[1]
-
-  # Extract date interval, considering multiple intervals inside dates_to_download
-  intervals = _find_date_intervals(dates_to_download)
-
-  for interval in intervals:
-
-    # Parse the date string using its current format
-    start_date_obj = datetime.strptime(interval[0], '%Y-%m-%d')
-    end_date_obj = datetime.strptime(interval[1], '%Y-%m-%d')
     
-    # Format the date objects to the BCB's REQUIRED format: MM-DD-YYYY
-    formatted_start_date = start_date_obj.strftime('%m-%d-%Y')
-    formatted_end_date = end_date_obj.strftime('%m-%d-%Y')
+    base_url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/"
+    all_downloaded_data = []
+    cleaned_downloaded_data = {}
 
-    endpoint = (
-        f"CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?"
-        f"@moeda='{currency}'&@dataInicial='{formatted_start_date}'&@dataFinalCotacao='{formatted_end_date}'&"
-        f"$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao")
-    
-    url = base_url + endpoint
+    # Extract currency code from partition key
+    currency = partition_key.split('#')[1]
 
-    # Execute the HTTP GET request
-    response = requests.get(url)
+    # Extract date interval, considering multiple intervals inside dates_to_download
+    intervals = _find_date_intervals(dates_to_download)
+        
+    for interval in intervals:
 
-    if response.status_code != 200:
-        raise Exception(f"Failed to fetch data: {response.status_code} - {response.text}")
-    
-    # Parse the JSON response
-    data = response.json()
-    
-    # Extract the actual data list which is usually under the 'value' key in OData responses
-    if 'value' in data:
-        all_downloaded_data.extend(data['value'])
+        # Parse the date string using its current format
+        start_date_obj = datetime.strptime(interval[0], '%Y-%m-%d')
+        end_date_obj = datetime.strptime(interval[1], '%Y-%m-%d')
+        
+        # Format the date objects to the BCB's REQUIRED format: MM-DD-YYYY
+        formatted_start_date = start_date_obj.strftime('%m-%d-%Y')
+        formatted_end_date = end_date_obj.strftime('%m-%d-%Y')
 
-    return all_downloaded_data
+        endpoint = (
+            f"CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?"
+            f"@moeda='{currency}'&@dataInicial='{formatted_start_date}'&@dataFinalCotacao='{formatted_end_date}'&"
+            f"$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao")
+        
+        url = base_url + endpoint
+
+        # Execute the HTTP GET request
+        response = requests.get(url)
+
+        # Parse the JSON response
+        data = response.json()
+        
+        # Extract the actual data list which is usually under the 'value' key in OData responses
+        if 'value' in data:
+            all_downloaded_data.extend(data['value'])
+
+    for value in all_downloaded_data:
+        
+        # Extract the date
+        date = value['dataHoraCotacao'].split(' ')[0]
+        
+        if date not in cleaned_downloaded_data or value['dataHoraCotacao'] > cleaned_downloaded_data[date]['dataHoraCotacao']:
+
+            # Turning into decimal to avoid breaking the results
+            value['ask_price'] = Decimal(str(value['cotacaoVenda']))
+            value['bid_price'] = Decimal(str(value['cotacaoCompra']))
+
+            value['mid_price'] = (value['ask_price'] + value['bid_price']) / 2
+            value['spread'] = value['ask_price'] - value['bid_price']
+
+            cleaned_downloaded_data[date] = value
+
+    desired_keys = ['ask_price', 'bid_price', 'mid_price', 'spread']
+
+    # Build currency_info for each date
+    currency_info = {
+        date: {key: cleaned_downloaded_data[date][key] 
+               for key in desired_keys 
+               if key in cleaned_downloaded_data[date]}
+        for date in cleaned_downloaded_data
+    }
+
+    return currency_info
 
 def lambda_handler(event, context):
     # Iterate through each record in the event
